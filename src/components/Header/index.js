c7fed4c182f781322ed28a58f1dbb2d40afb2405
github <rafael10elt@gmail.com>
2020-03-20 23:50:56 -0300

    v0.0.1

---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import "./styles.css"
+// criando componente Header
+const Header = () => (
+    <header id="main-header">Products List from API</header>
+);
+export default Header;
